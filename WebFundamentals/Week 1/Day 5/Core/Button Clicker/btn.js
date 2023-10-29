9f56de94b7febf59ac7358e1271aa1843b9ab421
anisrouis95 <anis.rouis@esprit.tn>
2023-10-29 19:24:27 +0100

    Day5 Button Click

---
+++ WebFundamentals/Week 1/Day 5/Core/Button Clicker/btn.js
@@ -0,0 +1,9 @@
+function turnOff(element) {
+    element.innerText = "Logout";
+}
+function removeButton(element) {
+    element.remove();
+}
+function showAlert() {
+    alert("Ninja was liked");
+}
