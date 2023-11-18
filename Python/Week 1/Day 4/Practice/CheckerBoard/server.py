29f23357ad37bffae991a652ec7d5e5e445884c6
anisrouis95 <anis.rouis@esprit.tn>
2023-11-18 22:04:05 +0100

    commit

--- Python/Week 1/Day 4/Practice/CheckerBoard/server.py
+++ Python/Week 1/Day 4/Practice/CheckerBoard/server.py
@@ -3,14 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # http://127.0.0.1:5001/
-@app.route('/')
-def index():
-    return render_template("index.html", number=8)
-
-# http://127.0.0.1:5001/4
-@app.route('/<int:number>')
-def rectangle(number):
-    return render_template("index.html", number=number)
+@app.route('/<int:x>/<int:y>/<color>/<color_1>')
+def index(x,y,color,color_1):
+    return render_template("index.html", x=x, y=y, color=color, color_1=color_1)
 
 
 
