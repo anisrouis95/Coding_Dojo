bde0ea5ec6e06578aefdbd992f9d6a1b28c0922c
anisrouis95 <anis.rouis@esprit.tn>
2023-12-18 11:30:19 +0100

    add mern stack

---
+++ Mern/Week_1/Day_1/01-scope.js
@@ -0,0 +1,12 @@
+var beatles = ['Spiderman', 'Batman', 'Jedi'];
+function printHero(list) {
+    console.log(list[0]);
+}
+
+printHero(superHeros)
+console.log("***************************");
+for(var i=0; i<superHeros.length;i++){
+    var hero = superHeros[i]
+    console.log(hero);
+}
+
