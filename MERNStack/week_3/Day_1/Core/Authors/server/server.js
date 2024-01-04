24fc29d9efa412d4d3bb8c11ccfbce9b0206e073
anisrouis95 <anis.rouis@esprit.tn>
2024-01-04 23:15:12 +0100

    Authors Core

---
+++ MERNStack/week_3/Day_1/Core/Authors/server/server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+
+// Middlewares
+app.use(cors(), express.json(), express.urlencoded({extended:true}));
+
+require ("dotenv").config()
+const PORT = process.env.PORT;
+
+// DB Connection
+require ("./config/mongoose.config");
+
+// Routes 
+require("./routes/authors.routes")(app);
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT} for requests 🚀`));
