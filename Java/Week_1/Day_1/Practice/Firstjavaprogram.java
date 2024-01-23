71bf3820b0e210c078ba7b2d3aab7556e20f0b21
anisrouis95 <anis.rouis@esprit.tn>
2024-01-23 10:25:57 +0100

    First java Program Practice

---
+++ Java/Week_1/Day_1/Practice/Firstjavaprogram.java
@@ -0,0 +1,8 @@
+public class Firstjavaprogram{
+    public static void main(String[] args){
+        String newline = System.lineSeparator(); 
+  
+        // Printing new line 
+        System.out.println("My name is Anis Rouis" + newline + "I am 28 years old" + newline + "My hometown is Ariana"); 
+    }
+}
