3a0186f7a9593ca15c755ad634c92a7dc902c35f
anisrouis95 <anis.rouis@esprit.tn>
2024-01-23 10:42:22 +0100

    Changes NewLine Fuctionality

--- Java/Week_1/Day_1/Practice/Firstjavaprogram.java
+++ Java/Week_1/Day_1/Practice/Firstjavaprogram.java
@@ -1,8 +1,5 @@
 public class Firstjavaprogram{
     public static void main(String[] args){
-        String newline = System.lineSeparator(); 
-  
-        // Printing new line 
-        System.out.println("My name is Anis Rouis" + newline + "I am 28 years old" + newline + "My hometown is Ariana"); 
+        System.out.println("My name is Anis Rouis" + \n + "I am 28 years old" + \n + "My hometown is Ariana"); 
     }
 }
