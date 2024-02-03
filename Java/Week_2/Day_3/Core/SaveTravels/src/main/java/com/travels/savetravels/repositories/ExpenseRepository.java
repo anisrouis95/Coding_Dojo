834dda66f69490e3bb78dad078f8d54f99c392bb
anisrouis95 <anis.rouis@esprit.tn>
2024-02-03 18:28:56 +0100

    Still working on this Assignment Got and Error but will be sorted

--- Java/Week_2/Day_3/Core/SaveTravels/src/main/java/com/travels/savetravels/repositories/ExpenseRepository.java
+++ Java/Week_2/Day_3/Core/SaveTravels/src/main/java/com/travels/savetravels/repositories/ExpenseRepository.java
@@ -1,2 +1,8 @@
-package com.travels.savetravels.repositories;public class ExpenseRepository {
+package com.travels.savetravels.repositories;
+
+import com.travels.savetravels.models.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 }
